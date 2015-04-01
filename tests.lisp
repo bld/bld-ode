8b2d2b7554bf2941e43beb12468de210bcb24f99
Ben Diedrich <bldiedrich@gmail.com>
2015-04-01 11:47:57 -0700

    Unit test for RKA

--- tests.lisp
+++ tests.lisp
@@ -4,3 +4,13 @@
 (defpackage :bld-ode-tests
   (:use :cl :bld-ode :fiveam))
 
+(def-suite :bld-ode)
+
+(in-suite :bld-ode)
+
+(test rka
+  (is-true (let* ((tol 1d-11)
+		  (results (rka #'(lambda (tm x p) (cos tm)) 0 pi 0 :tol tol)))
+	     (every #'identity
+		    (loop for (tm x) in results
+		       collect (< (abs (- x (sin tm))) tol))))))
